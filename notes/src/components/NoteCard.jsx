0a9d9eba6ed031e298db8cb54c433ae15ba30d99
thevaishndra <chandrav306m@gmail.com>
2024-10-16 13:54:24 +0530

    basic set up

--- notes/src/components/NoteCard.jsx
+++ notes/src/components/NoteCard.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
-
+import {useRef, useEffect} from 'react'
+import Trash from '../icons/trash';
 const NoteCard = ({note}) => {
     let position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors)
     const body = JSON.parse(note.body);
+
+    const textAreaRef = useRef(null);
+    useEffect(() => {
+      autoGrow(textAreaRef);
+    }, []);
+
+    const autoGrow = (textarea) => {
+      const { current } = textAreaRef;
+      current.style.height = "auto"; //Reset the height
+      current.style.height = current.scrollHeight + "px"; // Set the new height
+    }
+
   return (
-    <div className ='card' 
-    style = {{
-        backgroundColor : colors.colorBody,
-        }} >
-            {body}
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
+        ></textarea>
+      </div>
     </div>
-  )
+  );
 }
 
 export default NoteCard
